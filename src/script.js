c5d2d2e54a4b8711f0cd0d70fbd1e59f3c8abd47
bumbeishvili <me@davidb.dev>
2022-03-25 14:14:43 +0400

    new files

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from 'gsap';
 
 /**
  * Debug
@@ -8,7 +9,7 @@ import * as dat from 'lil-gui'
 const gui = new dat.GUI()
 
 const parameters = {
-    materialColor: '#ffeded'
+    materialColor: '#aba9ff'
 }
 
 
@@ -70,11 +71,32 @@ mesh1.position.y = -objDistance * 0;
 mesh2.position.y = -objDistance * 1;
 mesh3.position.y = -objDistance * 2;
 
-mesh1.position.x = 2
-mesh2.position.x = -2
-mesh3.position.x = 2;
+mesh1.position.x = 1.5
+mesh2.position.x = -1.5
+mesh3.position.x = 1.5;
 
 scene.add(mesh1, mesh2, mesh3)
+
+// Particles
+const count = 200;
+const positions = new Float32Array(count * 3);
+const pointGeometry = new THREE.BufferGeometry();
+const pointMaterial = new THREE.PointsMaterial({
+    color: parameters.color,
+    size: 0.01,
+    sizeAttenuation: true
+})
+
+for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    positions[i3 + 0] = (Math.random() - 0.5) * 4
+    positions[i3 + 1] = -Math.random() * objDistance * 3 + 2
+    positions[i3 + 2] = Math.random() * 4
+}
+pointGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+const points = new THREE.Points(pointGeometry, pointMaterial);
+scene.add(points)
 
 /**
  * Sizes
@@ -102,9 +124,12 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -117,9 +142,26 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 let scrollY = window.scrollY;
+let currentSection = 0;
 window.addEventListener('scroll', () => {
     scrollY = window.scrollY;
-    console.log({ scrollY })
+    const newSection = scrollY / sizes.height;
+    const newCurrentSection = Math.round(newSection)
+
+    if (currentSection != newCurrentSection) {
+        currentSection = newCurrentSection;
+        gsap.to(sectionMeshes[currentSection].rotation, {
+            duration: 1.5,
+            ease: 'power2.inOut',
+            x: '+=6',
+            y: '+=3',
+            z: '+=1.5'
+        })
+
+        console.log('section changed')
+    }
+
+
 })
 
 const cursor = {
@@ -128,9 +170,8 @@ const cursor = {
 };
 
 window.addEventListener('mousemove', event => {
-    cursor.x = event.clientX / sizes.width-0.5;
-    cursor.y = -event.clientY / sizes.height+0.5;
-    console.log(cursor)
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -event.clientY / sizes.height + 0.5;
 })
 
 
@@ -138,19 +179,28 @@ window.addEventListener('mousemove', event => {
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0;
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
 
     sectionMeshes.forEach((mesh, index) => {
-        mesh.rotation.y = elapsedTime * 0.1;
-        mesh.rotation.x = elapsedTime * 0.12;
+        mesh.rotation.y += deltaTime * 0.1;
+        mesh.rotation.x += deltaTime * 0.12;
     })
 
 
     camera.position.y = -scrollY / sizes.height * objDistance;
-    camera.position.x = cursor.x/4;
-    
+    // camera.position.x = cursor.x/4;
+
+    const parallaxX = -cursor.x * 0.5;
+    const parallaxY = -cursor.y * 0.5;
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+
+
 
     // Render
     renderer.render(scene, camera)
@@ -165,6 +215,7 @@ tick()
 gui
     .addColor(parameters, 'materialColor')
     .onChange(() => {
-        material.color.set(parameters.materialColor)
+        material.color.set(parameters.materialColor);
+        pointMaterial.color.set(parameters.materialColor)
     })
 
