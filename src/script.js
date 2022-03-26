a246976434e3d4c7a81e3bfd7d155b4954be235b
bumbeishvili <me@davidb.dev>
2022-03-26 18:02:25 +0400

    Add files

--- src/script.js
+++ src/script.js
@@ -1,18 +1,56 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import gsap from 'gsap';
+import CANNON from 'cannon';
+
+console.log(CANNON)
+
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+
+const concreteMaterial = new CANNON.Material('concrete');
+const groundMaterial = new CANNON.Material('ground');
+const plasticMaterial = new CANNON.Material('plastic');
+
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+    concreteMaterial,
+    plasticMaterial,
+    {
+        friction: 0.3,
+        restitution: 0.7,
+    }
+);
+world.addContactMaterial(concretePlasticContactMaterial)
+
+
+
+// SPHERE
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: sphereShape,
+    material: plasticMaterial
+})
+
+world.addBody(sphereBody);
+
+// PLANE
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body()
+floorBody.material = concreteMaterial;
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
+floorBody.mass  = 0;
+floorBody.addShape(floorShape);
+world.addBody(floorBody);
 
 /**
  * Debug
  */
 const gui = new dat.GUI()
-
-const parameters = {
-    materialColor: '#aba9ff'
-}
-
-
 
 /**
  * Base
@@ -24,79 +62,69 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test cube
+ * Textures
  */
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
-directionalLight.position.set(1, 1, 0)
-scene.add(directionalLight);
-
-
-
-
-// Material
 const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
-const gradientTexture = textureLoader.load('textures/gradients/5.jpg')
-const material = new THREE.MeshToonMaterial({ color: parameters.materialColor })
-material.gradientMap = gradientTexture;
-gradientTexture.mipmap = false;
-gradientTexture.minFilter = THREE.NearestFilter;
-gradientTexture.magFilter = THREE.NearestFilter;
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.png',
+    '/textures/environmentMaps/0/nx.png',
+    '/textures/environmentMaps/0/py.png',
+    '/textures/environmentMaps/0/ny.png',
+    '/textures/environmentMaps/0/pz.png',
+    '/textures/environmentMaps/0/nz.png'
+])
 
-
-
-
-
-// Meshes
-const mesh1 = new THREE.Mesh(
-    new THREE.TorusGeometry(1, 0.4, 16, 100,),
-    material
+/**
+ * Test sphere
+ */
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+    })
 )
+sphere.castShadow = true
+sphere.position.y = 0.5
+scene.add(sphere)
 
-const mesh2 = new THREE.Mesh(
-    new THREE.ConeGeometry(1, 2, 32),
-    material
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#777777',
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+    })
 )
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
 
-const mesh3 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-    material
-)
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+scene.add(ambientLight)
 
-const sectionMeshes = [mesh1, mesh2, mesh3]
-
-const objDistance = 4;
-mesh1.position.y = -objDistance * 0;
-mesh2.position.y = -objDistance * 1;
-mesh3.position.y = -objDistance * 2;
-
-mesh1.position.x = 1.5
-mesh2.position.x = -1.5
-mesh3.position.x = 1.5;
-
-scene.add(mesh1, mesh2, mesh3)
-
-// Particles
-const count = 200;
-const positions = new Float32Array(count * 3);
-const pointGeometry = new THREE.BufferGeometry();
-const pointMaterial = new THREE.PointsMaterial({
-    color: parameters.color,
-    size: 0.01,
-    sizeAttenuation: true
-})
-
-for (let i = 0; i < count; i++) {
-    const i3 = i * 3;
-    positions[i3 + 0] = (Math.random() - 0.5) * 4
-    positions[i3 + 1] = -Math.random() * objDistance * 3 + 2
-    positions[i3 + 2] = Math.random() * 4
-}
-pointGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-
-const points = new THREE.Points(pointGeometry, pointMaterial);
-scene.add(points)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -124,83 +152,50 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
-const cameraGroup = new THREE.Group();
-scene.add(cameraGroup);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(- 3, 3, 3)
+scene.add(camera)
 
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 6
-cameraGroup.add(camera)
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true
+    canvas: canvas
 })
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-let scrollY = window.scrollY;
-let currentSection = 0;
-window.addEventListener('scroll', () => {
-    scrollY = window.scrollY;
-    const newSection = scrollY / sizes.height;
-    const newCurrentSection = Math.round(newSection)
-
-    if (currentSection != newCurrentSection) {
-        currentSection = newCurrentSection;
-        gsap.to(sectionMeshes[currentSection].rotation, {
-            duration: 1.5,
-            ease: 'power2.inOut',
-            x: '+=6',
-            y: '+=3',
-            z: '+=1.5'
-        })
-
-        console.log('section changed')
-    }
-
-
-})
-
-const cursor = {
-    x: 0,
-    y: 0
-};
-
-window.addEventListener('mousemove', event => {
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = -event.clientY / sizes.height + 0.5;
-})
-
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
-let previousTime = 0;
+let oldElapsedTime = 0;
+
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime;
-    previousTime = elapsedTime;
+    const delta = elapsedTime - oldElapsedTime;
+    oldElapsedTime = elapsedTime;;
 
-    sectionMeshes.forEach((mesh, index) => {
-        mesh.rotation.y += deltaTime * 0.1;
-        mesh.rotation.x += deltaTime * 0.12;
-    })
+    world.step(1 / 60, delta, 3)
+    console.log(sphereBody.position.y)
 
+    sphere.position.x = sphereBody.position.x;
+    sphere.position.y = sphereBody.position.y;
+    sphere.position.z = sphereBody.position.z;
 
-    camera.position.y = -scrollY / sizes.height * objDistance;
-    // camera.position.x = cursor.x/4;
+    sphere.position.copy(sphereBody.position);
 
-    const parallaxX = -cursor.x * 0.5;
-    const parallaxY = -cursor.y * 0.5;
-    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
-    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+    // Update Physics World
 
-
+    // Update controls
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
@@ -210,12 +205,3 @@ const tick = () => {
 }
 
 tick()
-
-
-gui
-    .addColor(parameters, 'materialColor')
-    .onChange(() => {
-        material.color.set(parameters.materialColor);
-        pointMaterial.color.set(parameters.materialColor)
-    })
-
